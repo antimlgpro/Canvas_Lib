2b817588860d0bcab427375735049c6fe5fcf9bf
Ludvig <ludvigvonreis@gmail.com>
2021-10-23 00:34:31 +0200

    Created gameObject class This will be the base for every object.

--- src/object/GameObject.ts
+++ src/object/GameObject.ts
@@ -1,1 +1,40 @@
-class GameObject {}
+import Common from "../core/Common";
+import Vector from "../math/Vector";
+import Vertices from "../math/Vertices";
+import Transform from "./components/Transform";
+import Component from "./components/Component";
+
+class GameObject {
+	name: string;
+	id: number;
+
+	components: Component[] = [];
+
+	vertices: Vertices[];
+
+	constructor(name: string, position: Vector, vertices?: Vertices[]) {
+		this.name = name;
+		this.id = Common.NextId();
+
+		this.vertices = vertices ?? [];
+
+		let transform = new Transform(this);
+		transform.SetPosition(position);
+		this.AddComponent(transform);
+	}
+
+	AddComponent(comp: Component) {
+		this.components.push(comp);
+	}
+
+	GetComponent(type: string) {
+		console.log(this.components);
+		for (const comp of this.components) {
+			if (comp.type === type) {
+				return comp;
+			}
+		}
+	}
+}
+
+export default GameObject;
