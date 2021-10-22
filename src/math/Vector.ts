109072791a44ed93b74225764077fcf60ec87d90
Ludvig <ludvigvonreis@gmail.com>
2021-10-22 17:42:37 +0200

    Added Vector. Added Arithmic functions. Added Static helper functions

--- src/math/Vector.ts
+++ src/math/Vector.ts
@@ -1,1 +1,96 @@
-class Vector {}
+class Vector {
+	x: number = 0;
+	y: number = 0;
+
+	constructor(x: number, y: number) {
+		this.x = x;
+		this.y = y;
+	}
+
+	/**
+	 * Check whether { x } is of type Vector or number.
+	 */
+	protected static isVector(x: Vector | number): boolean {
+		return typeof x === typeof Vector;
+	}
+
+	static Dist(a: Vector, b: Vector): number {
+		let squaredSum = Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2);
+		return Math.sqrt(squaredSum);
+	}
+
+	static Dot(a: Vector, b: Vector) {
+		return a.x * b.x + a.y * b.y;
+	}
+
+	static Magnitude(vector: Vector) {
+		return Math.sqrt(vector.x * vector.x + vector.y * vector.y);
+	}
+
+	static MagnitudeSquared(vector: Vector) {
+		return vector.x * vector.x + vector.y * vector.y;
+	}
+
+	static Normalize(vector: Vector) {
+		let magnitude = Vector.Magnitude(vector);
+		if (magnitude === 0) {
+			return Vector.zero;
+		}
+
+		return new Vector(vector.x / magnitude, vector.y / magnitude);
+	}
+
+	static zero = () => {
+		return new Vector(0, 0);
+	};
+	static up = () => {
+		return new Vector(0, 0);
+	};
+	static down = () => {
+		return new Vector(0, 0);
+	};
+	static left = () => {
+		return new Vector(0, 0);
+	};
+	static right = () => {
+		return new Vector(0, 0);
+	};
+
+	// #region arithmetic
+	static Add(a: Vector, b: Vector | number): Vector {
+		if (!Vector.isVector(b)) {
+			b = <number>b;
+			return new Vector(a.x + b, a.y + b);
+		}
+		b = <Vector>b;
+		return new Vector(a.x + b.x, a.y + b.x);
+	}
+
+	static Sub(a: Vector, b: Vector | number): Vector {
+		if (!Vector.isVector(b)) {
+			b = <number>b;
+			return new Vector(a.x - b, a.y - b);
+		}
+		b = <Vector>b;
+		return new Vector(a.x - b.x, a.y - b.x);
+	}
+
+	static Multiply(a: Vector, b: Vector | number): Vector {
+		if (!Vector.isVector(b)) {
+			b = <number>b;
+			return new Vector(a.x * b, a.y * b);
+		}
+		b = <Vector>b;
+		return new Vector(a.x * b.x, a.y * b.x);
+	}
+
+	static Divide(a: Vector, b: Vector | number): Vector {
+		if (!Vector.isVector(b)) {
+			b = <number>b;
+			return new Vector(a.x - b, a.y - b);
+		}
+		b = <Vector>b;
+		return new Vector(a.x - b.x, a.y - b.x);
+	}
+	// #endregion
+}
