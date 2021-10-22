66b30555f160e5e4da6c70b8b94334dbfe9e3525
Ludvig <ludvigvonreis@gmail.com>
2021-10-23 00:28:24 +0200

    Added update function to engine

--- src/core/Engine.ts
+++ src/core/Engine.ts
@@ -1,3 +1,27 @@
-class Engine {
-	constructor() {}
+interface Timing {
+	timestamp: number;
+	lastDelta: number;
+	lastElapsed: number;
 }
+
+interface EngineOptions {}
+
+class Engine {
+	modules: Module[];
+	timing: Timing;
+
+	constructor(options: EngineOptions) {
+		// todo add options
+	}
+
+	Update(delta?: number) {
+		let startTime = Date.now();
+		delta = delta || 1000 / 60;
+
+		this.timing.timestamp += delta;
+		this.timing.lastDelta = delta;
+	}
+}
+
+export default Engine;
+export { EngineOptions };
