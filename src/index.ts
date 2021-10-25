15003428d6ca1de6288425ecb554874c75cd2890
Ludvig <ludvigvonreis@gmail.com>
2021-10-25 16:47:51 +0200

    Added gameloop Added 2 gameobjects with rigidbody.

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import GameObject from "./object/GameObject";
 import Transform from "./object/components/Transform";
 import Render, { RenderOptions } from "./render/Render";
 import Vector from "./math/Vector";
+import Mathf from "./math/Mathf";
+import Rigidbody from "./object/components/Rigidbody";
 
 const engineOptions: EngineOptions = {};
 const renderOptions: RenderOptions = {
@@ -16,16 +18,26 @@ const renderOptions: RenderOptions = {
 var _Engine: Engine;
 var _Render: Render;
 
+const gameLoop = () => {
+	_Engine.Update();
+};
+
 window.onload = () => {
 	_Engine = new Engine(engineOptions);
 	_Render = new Render(renderOptions);
 
-	let rect = <GameObject>Primitives.Rectangle(100, 100, 100, 100);
-	_Render.bodies.push(rect);
+	_Engine.render = _Render;
 
-	let transform = <Transform>rect.GetComponent("Transform");
+	var a: GameObject;
+	a = <GameObject>Primitives.Rectangle(0, 0, 100, 100);
+	a.AddComponent(new Rigidbody(a));
+	_Engine.AddGameObject(a);
 
-	transform.SetPosition(new Vector(400, 400));
+	a = <GameObject>Primitives.Rectangle(100, 30, 75, 75);
+	a.AddComponent(new Rigidbody(a));
+	_Engine.AddGameObject(a);
 
 	_Render.Run();
+
+	setInterval(gameLoop, 1000 / 60);
 };
