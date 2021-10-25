f9bd5811d4bb6803e10406ac00190784ebceca5f
Ludvig <ludvigvonreis@gmail.com>
2021-10-25 16:50:32 +0200

    Added gravity. Added running gameobjects update methods.

--- src/core/Engine.ts
+++ src/core/Engine.ts
@@ -1,4 +1,10 @@
+import Vector from "../math/Vector";
+import Rigidbody from "../object/components/Rigidbody";
+import GameObject from "../object/GameObject";
+import Render from "../render/Render";
+
 interface Timing {
+	delta: number;
 	timestamp: number;
 	lastDelta: number;
 	lastElapsed: number;
@@ -8,18 +14,65 @@ interface EngineOptions {}
 
 class Engine {
 	modules: Module[];
-	timing: Timing;
+	timing: Timing = {
+		delta: 0,
+		timestamp: 0,
+		lastDelta: 0,
+		lastElapsed: 0,
+	};
+	gravity: Vector = new Vector(0, 1);
+	gravityScale: number = 0.001;
+
+	render: Render;
+	gameObjects: GameObject[];
 
 	constructor(options: EngineOptions) {
 		// todo add options
+		this.gameObjects = [];
+	}
+
+	AddGameObject(gameObject: GameObject) {
+		this.gameObjects.push(gameObject);
+
+		this.render.AddGameObject(gameObject);
 	}
 
 	Update(delta?: number) {
 		let startTime = Date.now();
-		delta = delta || 1000 / 60;
+		let deltaTime = delta || 1000 / 60;
 
-		this.timing.timestamp += delta;
-		this.timing.lastDelta = delta;
+		this.timing.delta = deltaTime;
+		this.timing.timestamp += deltaTime;
+		this.timing.lastDelta = deltaTime;
+
+		//this.ApplyGravity();
+
+		for (const go of this.gameObjects) {
+			const comps = go.GetComponents();
+			for (const comp of comps) {
+				if (comp.type == "Rigidbody") {
+					// This applies gravity
+					let body = <Rigidbody>comp;
+					let force = new Vector(
+						body.mass * this.gravity.x * this.gravityScale,
+						body.mass * this.gravity.y * this.gravityScale
+					);
+					body.AddForce(force);
+					//console.log(body.positionPrev);
+				}
+
+				comp.Update(deltaTime);
+			}
+		}
+
+		this.timing.lastElapsed = Date.now() - startTime;
+	}
+
+	protected ApplyGravity() {
+		for (const go of this.gameObjects) {
+			let rigidbody = <Rigidbody>go.GetComponent("Rigidbody");
+			if (!rigidbody) continue;
+		}
 	}
 }
 
