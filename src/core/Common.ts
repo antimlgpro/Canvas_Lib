c5efb3ead8af7d3e92aa01b24642fcb05fe1b005
Ludvig <ludvigvonreis@gmail.com>
2021-11-08 12:59:59 +0100

    Removed common

--- src/core/Common.ts
+++
@@ -1,9 +0,0 @@
-class Common {
-	static _nextId = 0;
-
-	static NextId() {
-		return Common._nextId++;
-	}
-}
-
-export default Common;
--- src/object/Composite.ts
+++ src/object/Composite.ts
@@ -1,18 +1,18 @@
-import Common from "../core/Common";
+import { getUUID } from "../util/util";
 import GameObject from "./GameObject";
 
 /* eslint-disable-next-line */
 interface CompositeOptions {}
 
 class Composite {
-	id: number;
+	uuid: string;
 	label: string;
 	parent: unknown;
 	gameObjects: GameObject[];
 
 	/* eslint-disable-next-line */
 	constructor(options: CompositeOptions) {
-		this.id = Common.NextId();
+		this.uuid = getUUID();
 	}
 }
 
