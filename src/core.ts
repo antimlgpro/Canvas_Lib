a6830c9b57f5831da3e74a74e25ee74571cc2a39
Ludvig <ludvigvonreis@gmail.com>
2021-11-08 15:11:18 +0100

    created core library export

---
+++ src/core.ts
@@ -0,0 +1,24 @@
+import Engine, { EngineOptions } from "./core/Engine";
+import Events from "./core/Events";
+import EngineModule from "./core/modules/EngineModule";
+import Primitives from "./factory/Primitives";
+import Mathf from "./math/Mathf";
+import Vector from "./math/Vector";
+import Vertices from "./math/Vertices";
+import GameObject, { ObjectOptions } from "./object/GameObject";
+import Render, { RenderOptions } from "./render/Render";
+
+export {
+	Engine,
+	EngineOptions,
+	EngineModule,
+	Events,
+	GameObject,
+	ObjectOptions,
+	Render,
+	RenderOptions,
+	Mathf,
+	Vector,
+	Vertices,
+	Primitives,
+};
