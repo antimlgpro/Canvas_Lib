e369e5fe708a48e1e58189cf7a15c2050818497f
Ludvig <ludvigvonreis@gmail.com>
2021-11-16 14:04:52 +0100

    Moved all functions outside of class Added angle to radians function

--- src/math/Mathf.ts
+++ src/math/Mathf.ts
@@ -1,30 +1,32 @@
-class Mathf {
-	static Clamp(value: number, min: number, max: number) {
-		return Math.max(min, Math.min(value, max));
-	}
+const Clamp = (value: number, min: number, max: number) => {
+	return Math.max(min, Math.min(value, max));
+};
 
-	static Clamp01(value: number) {
-		if (value < 0) {
-			return 0;
-		} else if (value > 1) {
-			return 1;
-		} else {
-			return value;
-		}
+const Clamp01 = (value: number) => {
+	if (value < 0) {
+		return 0;
+	} else if (value > 1) {
+		return 1;
+	} else {
+		return value;
 	}
+};
 
-	static Repeat(t: number, length: number) {
-		return Mathf.Clamp(t - Math.floor(t / length) * length, 0, length);
-	}
+const Repeat = (t: number, length: number) => {
+	return Clamp(t - Math.floor(t / length) * length, 0, length);
+};
 
-	static PingPong(t: number, length: number) {
-		t = Mathf.Repeat(t, length * 2);
-		return length - Math.abs(t - length);
-	}
+const PingPong = (t: number, length: number) => {
+	t = Repeat(t, length * 2);
+	return length - Math.abs(t - length);
+};
 
-	static Lerp(a: number, b: number, t: number) {
-		return a + (b - a) * t;
-	}
-}
+const Lerp = (a: number, b: number, t: number) => {
+	return a + (b - a) * t;
+};
 
-export default Mathf;
+const AngleToRadians = (degrees: number) => {
+	return degrees * (Math.PI / 180);
+};
+
+export { Clamp, Clamp01, Repeat, PingPong, Lerp, AngleToRadians };
