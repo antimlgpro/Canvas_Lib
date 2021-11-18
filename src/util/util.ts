db75d71cfb9b6674f193550080b5b35fd0d02297
Ludvig <ludvigvonreis@gmail.com>
2021-11-18 18:39:04 +0100

    Added a combinations utility. this returns every combination of an array

--- src/util/util.ts
+++ src/util/util.ts
@@ -3,4 +3,23 @@ const getUUID = () => {
 	return uuid;
 };
 
-export { getUUID };
+function* combinations<T>(
+	array: any[],
+	length: number
+): IterableIterator<any[]> {
+	for (let i = 0; i < array.length; i++) {
+		if (length === 1) {
+			yield [array[i]];
+		} else {
+			const remaining = combinations(
+				array.slice(i + 1, array.length),
+				length - 1
+			);
+			for (const next of remaining) {
+				yield [array[i], ...next];
+			}
+		}
+	}
+}
+
+export { getUUID, combinations };
