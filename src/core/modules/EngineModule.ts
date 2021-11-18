18065700e9467db136fe400b74b0225d7153d83a
Ludvig <ludvigvonreis@gmail.com>
2021-11-18 18:40:25 +0100

    Implemented engine module class

--- src/core/modules/EngineModule.ts
+++ src/core/modules/EngineModule.ts
@@ -1,3 +1,17 @@
-class EngineModule {}
+import Engine from "core/Engine";
+
+class EngineModule {
+	engine: Engine;
+	moduleName = "Base";
+
+	// This will run after engine has initialized
+	init() {
+		//
+	}
+
+	update() {
+		//
+	}
+}
 
 export default EngineModule;
